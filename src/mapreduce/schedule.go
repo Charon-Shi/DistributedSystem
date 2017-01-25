9820f56cb5c6073c35096eb930fb609478b8b102
Charlie-Shi <chaos.scl@gmail.com>
2017-01-25 01:58:52 -0500

    Finish code of part III, fail on test

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -6,13 +6,25 @@ import "fmt"
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
 	var nios int // number of inputs (for reduce) or outputs (for map)
+
 	switch phase {
 	case mapPhase:
 		ntasks = len(mr.files)
 		nios = mr.nReduce
+		mapDone := make(chan int,  ntasks)
+
+		for i := 0; i < ntasks; i++ {
+			go mr.processMap(mapDone, i)
+		}
+
 	case reducePhase:
 		ntasks = mr.nReduce
 		nios = len(mr.files)
+		reduceDone := make(chan int, mr.nReduce)
+
+		for i := 0; i < ntasks; i++ {
+			go mr.processReduce(reduceDone, i)
+		}
 	}
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
@@ -25,4 +37,46 @@ func (mr *Master) schedule(phase jobPhase) {
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+func (mr *Master) processMap(mapDone chan int, mapTaskId int) {
+	for {
+		worker := <-mr.registerChannel
+
+		workerArg := &DoTaskArgs{}
+
+		workerArg.File = mr.files[mapTaskId]
+		workerArg.JobName = mr.jobName
+		workerArg.NumOtherPhase = mr.nReduce
+		workerArg.Phase = "Map"
+		workerArg.TaskNumber = mapTaskId
+
+		if call(worker, "Worker.DoTask", workerArg, nil) == true {
+			mr.registerChannel <- worker
+			mapDone <- mapTaskId
+
+			return
+		}
+	}
+}
+
+func (mr *Master) processReduce(reduceDone chan int, reduceTaskId int) {
+	for {
+		worker := <-mr.registerChannel
+
+		workerArg := &DoTaskArgs{}
+
+		workerArg.File = mr.files[reduceTaskId]
+		workerArg.JobName = mr.jobName
+		workerArg.NumOtherPhase = len(mr.files)
+		workerArg.Phase = "Reduce"
+		workerArg.TaskNumber = reduceTaskId
+
+		if call(worker, "Worker.DoTask", workerArg, nil) == true {
+			mr.registerChannel <- worker
+			reduceDone <- reduceTaskId
+
+			return
+		}
+	}
 }
